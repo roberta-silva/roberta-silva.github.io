edf2e9f22768751b52eded226fe88890a67286cf
Roberta Silva <robertadasilva.contato@gmail.com>
2026-01-17 16:37:27 -0300

    Adicionado novos projetos e slide aos projetos

--- js/script.js
+++ js/script.js
@@ -7,3 +7,8 @@ if (window.SimpleAnime) {
 import Decoradores from "./decoradores.js";
 const decoradores = new Decoradores();
 decoradores.init();
+
+import SlideNav from "./slide.js";
+const slide = new SlideNav(".slide", ".slide-wrapper");
+slide.init();
+slide.addArrow(".prev", ".next");
