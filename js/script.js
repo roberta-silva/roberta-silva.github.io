7ded2c186a367066a9c08cbe15bc92aa7aa75db7
Roberta Silva <robertadasilva.contato@gmail.com>
2026-01-20 12:55:09 -0300

    ajuste responsividade

--- js/script.js
+++ js/script.js
@@ -3,12 +3,7 @@ if (window.SimpleAnime) {
   new SimpleAnime();
 }
 
-//decoradores
-import Decoradores from "./decoradores.js";
-const decoradores = new Decoradores();
-decoradores.init();
-
-import SlideNav from "./slide.js";
+import SlideNav from "./modules/slide.js";
 const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
 slide.addArrow(".prev", ".next");
