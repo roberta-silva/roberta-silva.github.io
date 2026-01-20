826735a786e19c090f8fca908650c819bc986b9a
Roberta Silva <robertadasilva.contato@gmail.com>
2026-01-20 13:14:37 -0300

    adiciona scroll suave

--- js/script.js
+++ js/script.js
@@ -3,7 +3,13 @@ if (window.SimpleAnime) {
   new SimpleAnime();
 }
 
+//slide projetos
 import SlideNav from "./modules/slide.js";
 const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
 slide.addArrow(".prev", ".next");
+
+//scroll suave
+import ScrollSuave from "./modules/scroll-suave.js";
+const scrollsuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollsuave.init();
